a63894981e3f0d22408a08b61141d2052126373c
a2ron-k <aaronkwah@gmail.com>
2023-03-26 15:39:26 +0800

    feat: add style template

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,34 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+      rel="stylesheet"
+    />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+    <!-- * Navigation Bar -->
+    <div class="row g-0">
+      <div class="col-sm-1 col-xxl-1" />
+      <div class="col-sm-10 col-xxl-10">
+        <TopNav />
+      </div>
+      <div class="col-sm-1 col-xxl-1" />
     </div>
+
+    <!-- !TODO - Remove before moving to PRODUCTION -->
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      &nbsp;
+      <RouterLink to="#">Test</RouterLink>
+      &nbsp;
+    </nav>
   </header>
 
   <RouterView />
