cf5a18cb8cb213244f224ff02940b33be8fbbe80
a2ron-k <aaronkwah@gmail.com>
2023-03-29 12:24:12 +0800

    feat: update navigation bar

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,7 +1,7 @@
 <template>
   <header>
     <div>
-      <nav class="navbar navbar-expand" style="text-shadow: 0px 0px 5px rgba(239, 239, 239, 1)">
+      <!-- <nav class="navbar navbar-expand" style="text-shadow: 0px 0px 5px rgba(239, 239, 239, 1)">
         <div class="container-fluid" style="height: 81px">
           <router-link class="navbar-brand nav-margin-left nav-brand" to="/">
             <div style="width: 127px; height: 81px">
@@ -9,6 +9,43 @@
   
             </div>
           </router-link>
+        </div>
+      </nav> -->
+      <nav class="navbar navbar-expand-lg bg-light" style="width: 100vw">
+        <div class="container-fluid py-3">
+          <router-link class="navbar-brand nav-margin-left nav-brand" to="/">
+            <div style="height: 30px">
+              <img src="../assets/img/Logo.png" alt="Light mode logo" />
+              <span class="fw-bold">GreenVenture</span>
+            </div>
+          </router-link>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div id="navbarNav" class="collapse navbar-collapse">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Vouchers/ Points</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Leaderboard</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Log out</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     </div>
