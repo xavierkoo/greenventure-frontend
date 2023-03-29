2bb3ceaeb654c0bdc507dfef0dbf3f67fc3524fe
a2ron-k <aaronkwah@gmail.com>
2023-03-29 10:02:04 +0800

    feat: add mission card

--- src/views/GreetingView.vue
+++ src/views/GreetingView.vue
@@ -1,17 +1,7 @@
+<template>
+  <div></div>
+</template>
+
 <script setup></script>
 
-<template>
-  <div>
-    <div class="row">
-      <div class="d-none d-sm-block col-sm-2"></div>
-      <div class="col-12">
-        <div class="text-center">
-          <h1>Welcome to GreenVentures!</h1>
-
-          Test
-        </div>
-      </div>
-      <div class="d-none d-sm-block col-sm-2"></div>
-    </div>
-  </div>
-</template>
+<style lang="scss" scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,58 @@
-<script setup></script>
-
 <template>
-  <main>Hello World</main>
+  <div>
+    <!-- This is the top header controller btns -->
+    <div class="row g-2 text-center">
+      <div class="col-1" />
+      <div class="col-5">
+        <button type="button" class="btn btn-primary" data-bs-toggle="button" aria-pressed="true">
+          Available
+        </button>
+      </div>
+      <div class="col-5">
+        <button type="button" class="btn btn-primary" data-bs-toggle="button" aria-pressed="false">
+          In Progress
+        </button>
+      </div>
+      <div class="col-1" />
+    </div>
+
+    <!-- Mission Card Component -->
+    <div
+      v-for="(item, index) in allMissions"
+      :key="index"
+      class="my-3 mx-3 py-3 pad-d card text-white"
+    >
+      <img class="card-img-top" src="../assets/img/test.jpg" alt="Title" />
+      <div class="card-body">
+        <h4 class="card-title">
+          <h5>{{ item.missionID }}</h5>
+        </h4>
+        <p class="card-text">{{ item.description }}</p>
+        <button type="button" class="btn primary-btn" data-bs-toggle="button" aria-pressed="false">
+          Accept
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return { allMissions: '' }
+  },
+
+  computed: {},
+  async beforeMount() {
+    // ### Dynamic Pull based on the userID (uncomment the btm)
+    // const userID = localStorage.getItem('userID')
+    // const response = await axios.get('http://127.0.0.1:5201/mission/{userID}')
+
+    // ### Testing Code
+    const response = await axios.get('http://127.0.0.1:5201/mission/2')
+    this.allMissions = response.data.data.missions
+  },
+  methods: {}
+}
+</script>
